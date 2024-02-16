9b2d59522e2d0b7cae12d1e6fb056194bef2a101
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-02-16 13:19:12 +0200

    Update Program.cs

--- Theme17Pr2/task1/Theme17Pr2/Program.cs
+++ Theme17Pr2/task1/Theme17Pr2/Program.cs
@@ -1,38 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Theme17Pr2
+namespace Theme17Pr3
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            //List<Employee> empl = new List<Employee>();
-            //empl.Add(new Employee("","",1234, 2));
-            //empl.Add(new Engineer("","",10000, 2, 15));
-            //foreach (var num in empl)
-            //{
-            //    num.Output();
-            //    Console.WriteLine();
-            //}
-            try
+            List<Employee> empl = new List<Employee>();
+            empl.Add(new Employee("Даниил", "Полозюк", new DateTime(2006,03,20), 10000));
+            empl.Add(new Employee("", "", new DateTime(2006, 03, 20), 13564));
+            empl.Add(new Employee("", "", new DateTime(2006, 03, 20), 12766));
+            empl.Add(new Employee("", "", new DateTime(2006, 03, 20), 8563));
+            empl.Add(new Employee("", "", new DateTime(2006, 03, 20), 1756));
+            empl.Add(new Manager("", "", new DateTime(2006, 03, 20), 1756,2000));
+
+            foreach (var elem in empl)
             {
-                Engineer eng1 = new Engineer("", "", 10000, 2, 25);
-                eng1.Output();
+                double k = 2;
+                double H = 1500; 
+                elem.Income(k, H);
+                elem.Output();
+                
+                Console.WriteLine();
             }
-
-
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Исключение: {ex.Message}");
-                Console.WriteLine($"Метод: {ex.TargetSite}");
-                Console.WriteLine($"Трассировка стека: {ex.StackTrace}");
-            }
-
             Console.ReadKey();
         }
     }
